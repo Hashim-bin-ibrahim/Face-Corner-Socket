56cc098d78fb58127597c2a440e0ff4ab427df67
hashim <hashim@pop-os.localdomain>
2022-11-03 15:26:42 +0530

    localhost issue fixded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const io = require("socket.io")(9000, {
+const io = require("socket.io")(process.env.PORT, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONT_END_URL,
   },
 });
 
