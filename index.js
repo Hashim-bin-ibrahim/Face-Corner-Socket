8bc083060d6b957e521c43bbac4387062d7a8faf
hashim <hashim@pop-os.localdomain>
2022-11-22 11:00:10 +0530

    project completed :

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const io = require("socket.io")(process.env.PORT, {
+const io = require("socket.io")(9000, {
   cors: {
-    origin: process.env.FRONT_END_URL,
+    origin: "http://localhost:3000",
   },
 });
 
@@ -21,18 +21,20 @@ const getUser = (userId) => {
 
 io.on("connection", (socket) => {
   //when ceonnect
+  console.log("a user connected.");
 
   //take userId and socketId from user
   socket.on("addUser", (userId) => {
     addUser(userId, socket.id);
-
     io.emit("getUsers", users);
   });
 
   //send and get message
-  socket.on("sendMessage", ({ senderId, recieverId, text }) => {
-    const user = getUser(recieverId);
-    io.to(user?.socketId).emit("getMessage", {
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    console.log(senderId, receiverId, text);
+    const user = getUser(receiverId);
+    console.log("user  :", user);
+    io.to(user.socketId).emit("getMessage", {
       senderId,
       text,
     });
